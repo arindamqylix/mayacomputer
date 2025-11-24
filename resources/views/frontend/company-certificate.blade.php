c42facfeb549315075acba4b8e8ded3046b21973
Arindam <abera0275@gmail.com>
2025-11-24 18:09:53 +0530

    pdf add

---
+++ resources/views/frontend/company-certificate.blade.php
@@ -0,0 +1,25 @@
+@extends('frontend.layouts.master')
+@section('title','Home')
+@section('content')
+
+<div class="container mt-4 mb-5">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+
+            <div class="card shadow-sm border-0">
+                <div class="card-body p-3">
+
+                    <iframe src="{{ asset('frontend/document/MAYA COMPUTER CENTER PRIVATE LIMITED QRO EGAC 9001_231229_093548.pdf') }}" 
+                        width="100%" 
+                        height="800px"
+                        style="border: none;">
+                    </iframe>
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+@endsection
