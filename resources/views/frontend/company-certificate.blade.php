02793398add72f5c627601e306feb7cb8c911ebe
Arindam <abera0275@gmail.com>
2025-11-24 18:12:02 +0530

    update

--- resources/views/frontend/company-certificate.blade.php
+++ resources/views/frontend/company-certificate.blade.php
@@ -9,11 +9,11 @@
             <div class="card shadow-sm border-0">
                 <div class="card-body p-3">
 
-                    <iframe src="{{ asset('frontend/document/MAYA COMPUTER CENTER PRIVATE LIMITED QRO EGAC 9001_231229_093548.pdf') }}" 
+                    <!-- <iframe src="{{ asset('frontend/document/MAYA COMPUTER CENTER PRIVATE LIMITED QRO EGAC 9001_231229_093548.pdf') }}" 
                         width="100%" 
                         height="800px"
                         style="border: none;">
-                    </iframe>
+                    </iframe> -->
 
                 </div>
             </div>
